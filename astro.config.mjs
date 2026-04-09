296187c05c772c9bcd3f8f2cb4ebe05c328998b8
Sean Buckle <seanlbuckle@gmail.com>
2026-04-09 18:42:57 +0100

    chore: create astro.config.mjs for site configuration and integrations

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import compressor from 'astro-compressor';
+import { browserslistToTargets } from 'lightningcss';
+import browserslist from 'browserslist';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://rest-countries.seanbuckle.com',
+  base: '/',
+  output: 'static',
+  prefetch: true,
+  integrations: [sitemap(), compressor({ brotli: true })],
+  image: {
+    domains: ['flagcdn.net', 'upload.wikimedia.org', 'flagcdn.com'],
+  },
+  vite: {
+    optimizeDeps: {
+      include: [
+        'astro/toolbar',
+        'astro/runtime/client/dev-toolbar/entrypoint.js'
+      ]
+    },
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: browserslistToTargets(browserslist())
+      }
+    },
+    build: {
+      cssMinify: 'lightningcss'
+    }
+  },
+  experimental: {
+    rustCompiler: true,
+    
+  }
+});
